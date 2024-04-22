86341baf80bf535edf754062726b99f7b12bd943
Yasmin Dias <yasmindias.info@gmail.com>
2024-04-22 15:51:17 -0300

    Adiciona rotas de put e delete

--- src/app.js
+++ src/app.js
@@ -11,21 +11,43 @@ app.get("/livros", (req, res) => {
     res.status(200).json(livros)
 });
 
+app.get("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    res.status(200).json(livros[index])
+})
+
 app.post("/livros", (req, res) => {
     livros.push(req.body);
     res.status(201).send("Livro cadastrado com sucesso");
 });
 
+app.put("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo
+    res.status(200).json(livros)
+})
+
+app.delete('/livros/:id', (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros.splice(index, 1);
+    res.status(200).send('Livro removido com sucesso.');
+});
+
 const livros = [
     {
         id: 1,
-        título: "O Senhor dos Anéis"
+        titulo: "O Senhor dos Anéis"
     },
     {
         id: 2,
-        título: "O Hobbit"
+        titulo: "O Hobbit"
     }
 ];
 
+function buscaLivro(id) {
+    return livros.findIndex(livro => {
+        return livro.id === Number(id);
+    })
+}
 
 export default app
