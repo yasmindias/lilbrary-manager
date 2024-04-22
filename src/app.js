5c408fa2a655154c074cc23fa770ddc0c1b6c04f
Yasmin Dias <yasmindias.info@gmail.com>
2024-04-22 15:41:15 -0300

    Adiciona rota de cadastro no projeto

--- src/app.js
+++ src/app.js
@@ -1,9 +1,31 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
- res.status(200).send("Curso de Node.js")
+    res.status(200).send("Curso de Node.js")
 });
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros)
+});
+
+app.post("/livros", (req, res) => {
+    livros.push(req.body);
+    res.status(201).send("Livro cadastrado com sucesso");
+});
+
+const livros = [
+    {
+        id: 1,
+        título: "O Senhor dos Anéis"
+    },
+    {
+        id: 2,
+        título: "O Hobbit"
+    }
+];
+
 
 export default app
